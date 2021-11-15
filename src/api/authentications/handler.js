1f038fd10b972f7aca6163bb0745b2dabf9428e8
Ibrahim Syah <ibrahim.syah.q@gmail.com>
2021-11-15 14:26:41 +0700

    authorization implemented

--- src/api/authentications/handler.js
+++ src/api/authentications/handler.js
@@ -1,5 +1,5 @@
 const ClientError = require('../../exceptions/ClientError');
- 
+
 class AuthenticationsHandler {
   constructor(authenticationsService, usersService, tokenManager, validator) {
     this._authenticationsService = authenticationsService;
@@ -11,19 +11,19 @@ class AuthenticationsHandler {
     this.putAuthenticationHandler = this.putAuthenticationHandler.bind(this);
     this.deleteAuthenticationHandler = this.deleteAuthenticationHandler.bind(this);
   }
- 
+
   async postAuthenticationHandler(request, h) {
     try {
       this._validator.validatePostAuthenticationPayload(request.payload);
- 
+
       const { username, password } = request.payload;
       const id = await this._usersService.verifyUserCredential(username, password);
- 
+
       const accessToken = this._tokenManager.generateAccessToken({ id });
       const refreshToken = this._tokenManager.generateRefreshToken({ id });
- 
+
       await this._authenticationsService.addRefreshToken(refreshToken);
- 
+
       const response = h.response({
         status: 'success',
         message: 'Authentication berhasil ditambahkan',
@@ -43,7 +43,7 @@ class AuthenticationsHandler {
         response.code(error.statusCode);
         return response;
       }
- 
+
       // Server ERROR!
       const response = h.response({
         status: 'error',
@@ -58,11 +58,11 @@ class AuthenticationsHandler {
   async putAuthenticationHandler(request, h) {
     try {
       this._validator.validatePutAuthenticationPayload(request.payload);
- 
+
       const { refreshToken } = request.payload;
       await this._authenticationsService.verifyRefreshToken(refreshToken);
       const { id } = this._tokenManager.verifyRefreshToken(refreshToken);
- 
+
       const accessToken = this._tokenManager.generateAccessToken({ id });
       return {
         status: 'success',
@@ -80,7 +80,7 @@ class AuthenticationsHandler {
         response.code(error.statusCode);
         return response;
       }
- 
+
       // Server ERROR!
       const response = h.response({
         status: 'error',
@@ -95,11 +95,11 @@ class AuthenticationsHandler {
   async deleteAuthenticationHandler(request, h) {
     try {
       this._validator.validateDeleteAuthenticationPayload(request.payload);
- 
+
       const { refreshToken } = request.payload;
       await this._authenticationsService.verifyRefreshToken(refreshToken);
       await this._authenticationsService.deleteRefreshToken(refreshToken);
- 
+
       return {
         status: 'success',
         message: 'Refresh token berhasil dihapus',
@@ -113,7 +113,7 @@ class AuthenticationsHandler {
         response.code(error.statusCode);
         return response;
       }
- 
+
       // Server ERROR!
       const response = h.response({
         status: 'error',
--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -4,12 +4,12 @@ const bcrypt = require('bcrypt');
 const InvariantError = require('../../exceptions/InvariantError');
 const NotFoundError = require('../../exceptions/NotFoundError');
 const AuthenticationError = require('../../exceptions/AuthenticationError');
- 
+
 class UsersService {
   constructor() {
     this._pool = new Pool();
   }
- 
+
   async addUser({ username, password, fullname }) {
     await this.verifyNewUsername(username);
 
@@ -19,9 +19,9 @@ class UsersService {
       text: 'INSERT INTO users VALUES($1, $2, $3, $4) RETURNING id',
       values: [id, username, hashedPassword, fullname],
     };
- 
+
     const result = await this._pool.query(query);
- 
+
     if (!result.rows.length) {
       throw new InvariantError('User gagal ditambahkan');
     }
@@ -33,11 +33,11 @@ class UsersService {
       text: 'SELECT username FROM users WHERE username = $1',
       values: [username],
     };
- 
+
     const result = await this._pool.query(query);
- 
+
     if (result.rows.length > 0) {
-      throw new InvariantError('Gagal menambahkan user. Username sudah digunakan.')
+      throw new InvariantError('Gagal menambahkan user. Username sudah digunakan.');
     }
   }
 
@@ -46,13 +46,13 @@ class UsersService {
       text: 'SELECT id, username, fullname FROM users WHERE id = $1',
       values: [userId],
     };
- 
+
     const result = await this._pool.query(query);
- 
+
     if (!result.rows.length) {
       throw new NotFoundError('User tidak ditemukan');
     }
- 
+
     return result.rows[0];
   }
 
@@ -61,23 +61,22 @@ class UsersService {
       text: 'SELECT id, password FROM users WHERE username = $1',
       values: [username],
     };
- 
+
     const result = await this._pool.query(query);
- 
+
     if (!result.rows.length) {
       throw new AuthenticationError('Kredensial yang Anda berikan salah');
     }
- 
+
     const { id, password: hashedPassword } = result.rows[0];
- 
+
     const match = await bcrypt.compare(password, hashedPassword);
- 
+
     if (!match) {
       throw new AuthenticationError('Kredensial yang Anda berikan salah');
     }
     return id;
   }
-
 }
 
 module.exports = UsersService;
